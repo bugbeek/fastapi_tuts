b23576af31d2d5484112a3b2682014a38ce3dce0
Ashu <ashuxml@gmail.com>
2022-04-04 00:04:45 +0530

    commit 03042022

--- working.py
+++ working.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 def home():
-    return {"Data": "Test12"}
+    return {"Data": "This is your home section"}
+
+@app.get('/about')
+def about():
+    return {"Data":"This is your about section"}
+
+@app.post('/createpost')
+def create_post(payload : dict = Body(...)):
+    print(payload)
+    return {"Data": "This is your create post section"}
